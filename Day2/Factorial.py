d90a0779250355df89d14ac17d960411a2061dfd
NJOROMBE <njorombe@gmail.com>
2018-09-21 10:50:45 -0500

    Factorial of N
    These are entirely opinionated approaches
    
    * Prefer closing if's over if else. Deal with special cases first and proceed with the normal
    * return an error or raise exceptions. That way we no longer need if else, and the calling function print(factorial(n)) knows it is responsible to validate n

--- Day2/Factorial.py
+++ Day2/Factorial.py
@@ -1,10 +1,10 @@
 def factorial(n):
     if n < 0:
-        print('Cannot compute factorial of negative numbers')
-    elif n == 0:
+        raise Exception("Cannot compute factorial of negative numbers")
+        # return 'Cannot compute factorial of negative numbers'
+    if n == 0:
         return 1
-    else:
-        return n * factorial(n - 1)
+    return n * factorial(n - 1)
 
 
 n = int(input("Enter a number to compute factorial:"))
